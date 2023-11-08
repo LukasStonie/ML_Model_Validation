bd957b9e67dc0b00470f660a0a374a926b8096e9
Lukas Steininger <56015465+LukasStonie@users.noreply.github.com>
2023-11-08 11:31:15 +0100

    implement export to csv

--- _lib/export.py
+++ _lib/export.py
@@ -4,8 +4,19 @@ import string
 import pandas as pd
 
 
-def to_csv(data: dict, path: string):
+def to_csv(data: dict, path: string, scoring='accuracy', param_opt=False):
+    multi_score = not isinstance(scoring, str)
     if not os.path.exists(path):
         os.makedirs(path)
-    for k, v in data.items():
-        pd.DataFrame(v).to_csv(os.path.join(path, k + ".csv"))
+    if param_opt:
+        pd.DataFrame(data['mean_fit_time']).to_csv(os.path.join(path, "mean_fit_time.csv"))
+        pd.DataFrame(data['mean_score_time']).to_csv(os.path.join(path, "mean_score_time.csv"))
+        for s in scoring:
+            pd.DataFrame(data['mean_test_'+s]).to_csv(os.path.join(path, 'mean_test_'+s + ".csv"))
+            pd.DataFrame(data['mean_train_'+s]).to_csv(os.path.join(path, 'mean_train_'+s + ".csv"))
+    else:
+        pd.DataFrame(data['fit_time']).to_csv(os.path.join(path, "mean_fit_time.csv"))
+        pd.DataFrame(data['score_time']).to_csv(os.path.join(path, "mean_score_time.csv"))
+        for s in scoring:
+            pd.DataFrame(data['test_'+s]).to_csv(os.path.join(path, 'mean_test_'+s + ".csv"))
+            pd.DataFrame(data['train_'+s]).to_csv(os.path.join(path, 'mean_train_'+s + ".csv"))
